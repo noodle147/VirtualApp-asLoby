2d22a4b4924d8e3979ad86a1f1913be465ab54fd
Jiele Ye <yejiele1983@hotmail.com>
2021-09-16 11:08:01 +0800

    fix build break

--- VirtualApp/lib/src/main/jni/Application.mk
+++ VirtualApp/lib/src/main/jni/Application.mk
@@ -1,6 +1,8 @@
-APP_ABI := armeabi-v7a x86
+# APP_ABI := armeabi-v7a x86 arm64-v8a
+APP_ABI := armeabi-v7a
 APP_PLATFORM := android-14
-APP_STL := gnustl_static
+# APP_STL := gnustl_static
+APP_STL := c++_static
 APP_OPTIM := release
 VA_ROOT          := $(call my-dir)
 NDK_MODULE_PATH  := $(NDK_MODULE_PATH):$(VA_ROOT)
